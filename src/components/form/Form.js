326de617d0b3f7acf68de9a73c9a6d242aca3b4d
Ajayi Emmanuel Tomiwa <ajayiemmanuel630@gmail.com>
2021-08-04 15:24:43 +0100

    Update Form.js

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,7 +1,7 @@
 import "./form.css";
 function Form() {
   return (
-    <form data-netlify="true" method="POST" name="contact_tomiwa" className="form">
+    <form data-netlify="true" method="POST" name="contact_tomiwa" onSubmit="submit" className="form">
       <input type="hidden" name="form-name" value="contact_tomiwa" />
       <div className="flex-form-grp">
         <div className="form-group">
